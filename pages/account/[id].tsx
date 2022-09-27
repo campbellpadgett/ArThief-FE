e22191f70f7b783c5ba5108c22cf990b90e90ef4
Campbell Padgett <campbellpadgett5@gmail.com>
2022-09-27 15:41:56 -0500

    added profile, need option for other users account

---
+++ pages/account/[id].tsx
@@ -0,0 +1,69 @@
+import type { NextPage } from 'next'
+import Grid from '@mui/material/Unstable_Grid2';
+import { Box } from '@mui/material';
+import Loading from '../../components/Loading';
+import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
+import { UserData } from '../../utils/checkUser';
+import {api} from '../../utils/keys'
+import { useRecoilValue } from 'recoil';
+import { userDataAtom } from '../../atoms/userAtoms';
+
+ const userReq = async (id: string): Promise<UserData | undefined> => {
+        const res = await fetch(`http://${api}/users`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(id)
+        }) 
+        
+        const currentUsereData: UserData = await res.json()
+        return currentUsereData
+    }
+
+const Login: NextPage = () => {
+
+    const router = useRouter();
+    const query = router.query;
+    const userID = query.id as string
+
+    const [user, setUser] = useState<UserData | undefined>(undefined)
+    const accountHolderData = useRecoilValue(userDataAtom)
+
+    useEffect(() => {
+        (async () => {
+            if (accountHolderData && userID == accountHolderData.ID.toString()) {
+                setUser(accountHolderData)
+                return
+            } 
+
+            const res = await userReq(userID)
+            if (res) setUser(res)
+        })()
+    }, [userID])
+
+
+    return (
+        <Grid container spacing={2}>
+            <Grid sm={4} md={4} lg={4}></Grid>
+            <Grid sm={4} md={4} lg={4}>
+
+
+                <Box sx={{margin: 'auto'}}>
+                   {user ? 
+
+                   <div>
+                        <div>{user.username}</div>
+                        <div>{user.email}</div>
+                   </div>
+                   
+                   : <Loading />}
+                </Box>
+
+
+            </Grid>
+            <Grid sm={4} md={4} lg={4}></Grid>
+        </Grid>
+    )
+}
+
+export default Login
