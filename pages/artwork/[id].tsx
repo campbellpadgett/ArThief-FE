1812b7216f180e8a64bbe14ba9a79a0a1713b399
Campbell Padgett <campbellpadgett5@gmail.com>
2022-10-06 15:58:08 -0500

    artwork more responsive for mobile devices

--- pages/artwork/[id].tsx
+++ pages/artwork/[id].tsx
@@ -5,10 +5,11 @@ import CardMedia from '@mui/material/CardMedia';
 import LikeButton from "../../components/LikeButton";
 import { useRecoilValue } from "recoil";
 import { userDataAtom } from "../../atoms/userAtoms";
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState, useEffect, useMemo } from 'react'
 import { NextPage } from "next";
 import {api} from '../../utils/keys'
 import { LikeData, LikeRequest } from "../../utils/interfaces";
+import { debounce } from "lodash";
 
 const rowSX = {
     padding: '2px',
@@ -22,21 +23,27 @@ const renderSource = (source: string, Abb: string) => {
 }
 
 const likeHandler = async (likedStatus: boolean, setStatus: Dispatch<SetStateAction<boolean>>, likeData: LikeData) => {
-        setStatus(!likedStatus)
+    setStatus(!likedStatus)
 
-        const ld: LikeData = {
-            itemID: likeData.itemID,
-            userID: likeData.userID,
-            likeStatus: !likedStatus
-        }
-
-        const res = await fetch(`http://${api}/like`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(ld)
-        })
+    const ld: LikeData = {
+        itemID: likeData.itemID,
+        userID: likeData.userID,
+        likeStatus: !likedStatus
     }
+
+    const res = await fetch(`http://${api}/like`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(ld)
+        })
+}
+
+const imgSize = (window: number) => {
+    if (window >= 1000) return '50%'
+    if (window <= 999 && window > 425) return '75%'
+    if (window <= 424) return '100%'
+}
 
 const Artwork: NextPage = () => {
     const router = useRouter();
@@ -57,6 +64,16 @@ const Artwork: NextPage = () => {
         return currentLikeData
     }
 
+    const [screenSize, setScreenSize] = useState<number>(576)
+    const resize = () => setScreenSize(window.innerWidth)
+    const handleResize = useMemo(() => debounce(resize, 100), [])
+
+    useEffect(() => {
+        setScreenSize(window.innerWidth)
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    })
 
     return (
         <div>
@@ -64,7 +81,7 @@ const Artwork: NextPage = () => {
             <Grid container spacing={1}>
                 <Grid xs={2} sm={2} md={2} />
                 <Grid xs={8} sm={8} md={8}>
-                    <Card sx={{ maxWidth: '40%', margin: 'auto' }}>
+                    <Card sx={{ maxWidth: imgSize(screenSize), margin: 'auto' }}>
                         <CardMedia
                         component="img"
                         // @ts-ignore
